7ec53d7e6e6d10812e665b1b9516c5b4503d6250
NasimYus <nasim_yusupov@bk.ru>
2022-08-06 17:32:16 +0500

    FEATURE: create backend

---
+++ src/components/core/navbar/SearchInput.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "searchInput"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/product/ProductPopularCard.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "ProductPopularCard"
+}
+</script>
+
+<style scoped>
+
+</style>
